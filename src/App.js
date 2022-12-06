1f91cda802ab73ad69573e0a80d0b6e4979479c3
sniiz <88880069+sniiz@users.noreply.github.com>
2022-12-06 22:11:34 +0300

    added the cassette

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [url, setUrl] = useState("");
+  const [spin, setSpin] = useState(false);
+  const startPlayback = (url) => {
+    if (!url) return;
+    console.log("i have been clicked");
+    setUrl("");
+    setSpin(true);
+  };
   return (
     <>
       <body>
@@ -21,12 +30,43 @@ function App() {
                 .focus();
             }}
           >
-            v0.0.0
+            VERY MUCH WIP
           </code>
         </div>
         <div className="input">
-          <input type="text" placeholder="enter a song name or a yt url" />
-          <div className="add-button">play</div>
+          <input
+            type="text"
+            placeholder="enter a song name or a yt url"
+            onChange={(e) => {
+              setUrl(e.target.value);
+            }}
+            value={url}
+          />
+          <div className="add-button" onClick={() => startPlayback(url.trim())}>
+            play
+          </div>
+        </div>
+        <div className="cassette-body">
+          <img
+            className={(spin ? "spinning " : "") + "cassette-rolls"}
+            src="./assets/roll.svg"
+            alt=""
+          />
+          <img
+            className={(spin ? "spinning " : "") + "cassette-rolls"}
+            src="./assets/roll.svg"
+            alt=""
+          />
+        </div>
+        <div className="controls">
+          <img
+            src={`./assets/${spin ? "pause" : "play"}.svg`}
+            alt=""
+            className="control-button"
+            onClick={() => {
+              setSpin(!spin);
+            }}
+          />
         </div>
       </body>
     </>
